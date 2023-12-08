ac60719960294b727af90e932280008be9d09bce
YoussefGhollamallah <youssef.gh@live.fr>
2023-12-08 16:04:37 +0100

    update

--- main.py
+++ main.py
@@ -3,36 +3,96 @@ from random import choice
 
 pygame.init()
 
-
-largeur, hauteur = 800, 800
+largeur, hauteur = 1000, 800
 screen = pygame.display.set_mode((largeur, hauteur))
 pygame.display.set_caption("Le Pendu")
 
+bg = (25, 35, 55)
+text_color = (255, 255, 255)
 
-noir = (25, 35, 55)
-blanc = (255, 255, 255)
+menu = False
 
+police = pygame.font.SysFont("arialblack", 30)
 
-police = pygame.font.SysFont("arial", 30)
+def menu_pendu(text, font, text_col, x, y):
+    message = font.render(text, True, text_col)
+    screen.blit(message, (x, y))
 
 def afficher_mot(mot, lettres_devinées):
     affichage = " ".join([lettre if lettre in lettres_devinées else "_" for lettre in mot])
-    texte = police.render(affichage, 1, blanc)
+    texte = police.render(affichage, 1, text_color)
     screen.blit(texte, (largeur // 4, hauteur // 2))
 
+def rejouer():
+    global menu
+    global max_erreurs
+    global mot_solution
+    global lettres_devinées
+
+    menu = False
+    max_erreurs = 6
+    lettres_devinées = set()
+    mot_solution = choice(mots).upper()
 
 with open("mots.txt", 'r') as fichier:
     mots = fichier.read().splitlines()
 
-
 mot_solution = choice(mots).upper()
-
 lettres_devinées = set()
-
 max_erreurs = 6
+
+def game():
+    global menu
+    global max_erreurs
+    global mot_solution
+    global lettres_devinées
+
+    screen.fill(bg)
+
+    if menu == False:
+        menu_pendu("Appuyer sur Espace pour jouer", police, text_color, 240, 360)
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    menu = True 
+    else:
+        afficher_mot(mot_solution, lettres_devinées)
+
+        lettres_texte = police.render(f"Lettres devinées: {' '.join(sorted(lettres_devinées))}", 1, text_color)
+        screen.blit(lettres_texte, (largeur // 4, hauteur // 4))
+
+            
+        if max_erreurs < 6:
+            pygame.draw.circle(screen, text_color, (3 * largeur // 4, hauteur // 4), 30, 2)
+        if max_erreurs < 5:
+            pygame.draw.line(screen, text_color, (3 * largeur // 4, hauteur // 4 + 30), (3 * largeur // 4, hauteur // 2), 2)  
+        if max_erreurs < 4:
+            pygame.draw.line(screen, text_color, (3 * largeur // 4, hauteur // 4 + 30), (3 * largeur // 4 - 20, hauteur // 2), 2)  
+        if max_erreurs < 3:
+            pygame.draw.line(screen, text_color, (3 * largeur // 4, hauteur // 4 + 30), (3 * largeur // 4 + 20, hauteur // 2), 2)
+        if max_erreurs < 2:
+            pygame.draw.line(screen, text_color, (3 * largeur // 4, hauteur // 2), (3 * largeur // 4 - 20, 3 * hauteur // 4), 2)  
+        if max_erreurs < 1:
+            pygame.draw.line(screen, text_color, (3 * largeur // 4, hauteur // 2), (3 * largeur // 4 + 20, 3 * hauteur // 4), 2)  
+
+
+        if set(lettres_devinées) >= set(mot_solution):
+            menu_pendu("Voulez vous rejouer (appuyez sur R)", police, text_color, 240, 360)
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_r]:  
+                rejouer()
+
+        elif max_erreurs == 0:
+            menu_pendu("Voulez vous rejouer (appuyez sur R)", police, text_color, 240, 360)
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_r]:
+                rejouer()
+
+    pygame.display.flip()
 
 running = True
 while running:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -44,36 +104,6 @@ while running:
                     if lettre not in mot_solution:
                         max_erreurs -= 1
 
-    screen.fill(noir)
-
-    afficher_mot(mot_solution, lettres_devinées)
-
-    lettres_texte = police.render(f"Lettres devinées: {' '.join(sorted(lettres_devinées))}", 1, blanc)
-    screen.blit(lettres_texte, (largeur // 4, hauteur // 4))
-
-    
-    if max_erreurs < 6:
-        pygame.draw.circle(screen, blanc, (3 * largeur // 4, hauteur // 4), 30, 2)
-    if max_erreurs < 5:
-        pygame.draw.line(screen, blanc, (3 * largeur // 4, hauteur // 4 + 30), (3 * largeur // 4, hauteur // 2), 2)  
-    if max_erreurs < 4:
-        pygame.draw.line(screen, blanc, (3 * largeur // 4, hauteur // 4 + 30), (3 * largeur // 4 - 20, hauteur // 2), 2)  
-    if max_erreurs < 3:
-        pygame.draw.line(screen, blanc, (3 * largeur // 4, hauteur // 4 + 30), (3 * largeur // 4 + 20, hauteur // 2), 2)
-    if max_erreurs < 2:
-        pygame.draw.line(screen, blanc, (3 * largeur // 4, hauteur // 2), (3 * largeur // 4 - 20, 3 * hauteur // 4), 2)  
-    if max_erreurs < 1:
-        pygame.draw.line(screen, blanc, (3 * largeur // 4, hauteur // 2), (3 * largeur // 4 + 20, 3 * hauteur // 4), 2)  
-
-   
-    pygame.display.flip()
-
-  
-    if set(lettres_devinées) >= set(mot_solution):
-        print("Félicitations ! Vous avez gagné !")
-        running = False
-    elif max_erreurs == 0:
-        print(f"Dommage ! Vous avez perdu. Le mot était '{mot_solution}'.")
-        running = False
+    game()
 
 pygame.quit()
